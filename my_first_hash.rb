b5cf8505d374ba6ad90a94bea69934999fd8779c
Clayton Moore <cmoorefour@gmail.com>
2020-07-09 21:36:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,5 @@
-def my_hash
+def my_hash = 
+   your_hash = { "car" => "Mustang" , "truck" => "Cadillac" , "bike" => "Mongoose" }
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
@@ -20,6 +21,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
+shipping_manifest = ["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
