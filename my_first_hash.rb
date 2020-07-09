e16fd00f587c5b4ba185e4fb230bef11131b69d5
Clayton Moore <cmoorefour@gmail.com>
2020-07-09 21:52:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,9 +20,9 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-shipping_manifest["oil paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -40,5 +40,5 @@ shipping_manifest = "muskets" , 2
 shipping_manifest = "gun powder", 4
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
